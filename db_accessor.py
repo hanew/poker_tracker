97fddecacb8876ae97d084a628ed317bbfac3f82
Hanew Alhayek <hanewalhayek@Hanews-MBP.home>
2020-01-15 15:38:41 -0500

    intiial commit

---
+++ db_accessor.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+
+import pymysql
+import datetime
+
+# Open database connection
+# db = pymysql.connect("localhost","root","coenjit1","sys" )
+
+# function that executes a sql select query
+# function input: query
+# function output: rows returned
+
+# biggest winner
+
+def get_biggest_winner():
+	# prepare a cursor object using cursor() method
+	cursor = db.cursor()
+
+	query = "SELECT max(net_gain) as biggest_winner from poker_app"
+
+	try:		
+		# execute SQL query using execute() method.
+		cursor.execute(query)
+		results = cursor.fetchone()
+		net_gain = results[0]
+		print ("biggest winnings = %4.2f" % (net_gain))
+		print(cursor.rowcount, "record returned.")
+
+	except:
+		print ("Error: unable to fetch data")
+
+def fixnetgain():
+	mycursor = db.cursor()
+	sql = "UPDATE poker_app SET net_gain = (cash_out - buy_in) WHERE net_gain = 0"
+
+	mycursor.execute(sql)
+
+	db.commit()
+
+	print(mycursor.rowcount, "record fixed.\n")
+
+
+def select_all():
+	db = pymysql.connect("localhost","root","coenjit1","sys" )
+	cursor = db.cursor(pymysql.cursors.DictCursor)
+	query = "SELECT * from poker_app"
+
+	cursor.execute(query)
+
+	rows = cursor.fetchall()
+	db.close()
+	#print(rows)
+	return rows
+
+def insert_row(buyin, cashout, location, bigblind, littleblind, startime, endtime):
+	db = pymysql.connect("localhost","root","coenjit1","sys" )
+
+	mycursor = db.cursor()
+	
+	sql = "INSERT INTO poker_app(buy_in, cash_out, net_gain, location, big_blind, little_blind, startime, endtime) values(%s, %s, %s, %s, %s, %s, %s, %s)"
+	val = (buyin, cashout, cashout-buyin, location, bigblind, littleblind, startime, endtime)
+	mycursor.execute(sql, val)
+
+	db.commit()
+
+	print(mycursor.rowcount, "record inserted.\n")
+	mycursor.close()
+	db.close()
+	return mycursor.rowcount
+
+def delete_row(startime):
+	cursor = db.cursor()
+	sql = "DELETE FROM poker_app WHERE startime = %s"
+	adr = (startime)
+
+	mycursor.execute(sql, adr)
+	db.commit()
+
+	print(mycursor.rowcount, "record inserted.\n")
+
+#fixnetgain()
+
+#db.close()
+
+	
---
+++ server.py
@@ -0,0 +1,59 @@
+from flask import Flask, request, Response, render_template
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+from json import dumps
+from flask_jsonpify import jsonify
+import db_accessor
+from flask_cors import CORS
+
+#db_connect = create_engine('sqlite:///chinook.db')
+app = Flask(__name__)
+api = Api(app)
+CORS(app)
+# class Employees(Resource):
+#     def get(self):
+#         conn = db_connect.connect() # connect to database
+#         query = conn.execute("select * from employees") # This line performs query and returns json result
+#         return {'employees': [i[0] for i in query.cursor.fetchall()]} # Fetches first column that is Employee ID
+
+# class Tracks(Resource):
+#     def get(self):
+#         conn = db_connect.connect()
+#         query = conn.execute("select trackid, name, composer, unitprice from tracks;")
+#         result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+#         return jsonify(result)
+
+# class Employees_Name(Resource):
+#     def get(self, employee_id):
+#         conn = db_connect.connect()
+#         query = conn.execute("select * from employees where EmployeeId =%d "  %int(employee_id))
+#         result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+#         return jsonify(result)
+@app.route('/', methods=['GET', 'POST'])
+
+class All_Data(Resource):
+    def get(self):
+        results = db_accessor.select_all()
+        return jsonify(results)
+
+class Add_Session(Resource):
+	def post(self):
+		import pdb; pdb.set_trace();
+		buyin = request.form['buyin']
+		cashout = request.form['cashout']
+		location = request.form['location']
+		bigblind = request.form['bigblind']
+		littleblind = request.form['littleblind']
+		startime = request.form['startime']
+		endtime = request.form['endtime']
+		results = db_accessor.insert_row(buyin,cashout,location,bigblind,littleblind,startime,endtime)
+		return jsonify(results)
+
+api.add_resource(All_Data, '/all') # Route_1
+api.add_resource(Add_Session, '/addSession') # Route_2
+# api.add_resource(Employees_Name, '/employees/<employee_id>') # Route_3
+
+
+if __name__ == '__main__':
+     app.run(port='5002')
+     
